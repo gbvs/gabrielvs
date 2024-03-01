99a794649ae45a0feaf27f4551a5a75fd5c80c48
gbvs <161864302+gbvs@users.noreply.github.com>
2024-03-01 17:51:44 +0100

    Add files via upload

---
+++ first.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>PHP First Example</title>
+<script>
+function hello(){
+	alert("Hello");
+ }
+ </script>
+ </head>
+ <body onload="hello()">
+ 	<p>Welcome to PHP</p>
+
+	
+   	This is a new line <br>
+
+  	<h1>Hello</h1> 
+ </body>
+</html>
