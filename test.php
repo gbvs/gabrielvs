3c1213e7b0a73c9dd77b59134e1085e4e26f4385
gbvs <161864302+gbvs@users.noreply.github.com>
2024-03-01 17:18:55 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Php Cours</title>
+</head>
+<body>
+    <h1>Welcome to the PHP</h1>
+    <?php
+        echo "Hello John";
+    ?>
+    <h1>The end of the page</h1>
+</body>
+</html>
