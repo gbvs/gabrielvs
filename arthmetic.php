0cee06e46e9c2f6401ab23fcf86d364c4bcd467c
gbvs <161864302+gbvs@users.noreply.github.com>
2024-03-01 17:20:13 +0100

    Add files via upload

---
+++ arthmetic.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Arthmical operations</h1>
+
+    <?php
+        $a = 234;
+        $b = 33;
+
+        $c = $a + $b;
+        echo "$a + $b = $c<br>";
+        
+       
+        $c = $a - $b;
+        echo "$a - $b = $c<br>";
+
+        $c = $a / $b;
+        echo "$a / $b = $c<br>";
+
+        $c = $a % $b;
+        echo "$a % $b = $c<br>";
+
+        $a++;
+
+        echo "Now a = $a";
+        
+        $b--;
+
+        echo "Now b = $b";
+
+    ?>   
+
+      
+</body>
+</html>
