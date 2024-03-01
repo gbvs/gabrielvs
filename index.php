bee8be2ea4bad7a2d2fba7f87c54093c137db0c1
gbvs <161864302+gbvs@users.noreply.github.com>
2024-03-01 17:50:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example</title>
+</head>
+<body>
+ <h2>Welcome to the cours 2</h2> 
+ <?php
+ $first_name = "John";
+ $last_name = "Doe";
+
+ $name = $first_name . " " . $last_name;
+ 
+ $age = 34;
+ 
+
+ echo "Your name is $name <br>";
+
+ echo "Your age is $age";
+ 
+ ?>
+</body>
+</html>
